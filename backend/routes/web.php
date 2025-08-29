1d5dfcab7f5dd5bb351d6cb7e558775559deee46
lucas00miranda <lucas.miranda@quatainvestimentos.com.br>
2025-08-28 23:43:12 -0300

    feat (frontend) : set up front

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,13 +1,1 @@
 <?php
-
-use App\Http\Controllers\ReservationController;
-use App\Http\Controllers\RoomController;
-use App\Http\Controllers\UserController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/rooms', [RoomController::class, 'index']);
-Route::get('/users', [UserController::class, 'index']);
-
-Route::get('/reservations', [ReservationController::class, 'index']);
-Route::post('/reservations', [ReservationController::class, 'store']);
-Route::delete('/reservations/{reservation}', [ReservationController::class, 'destroy']);
